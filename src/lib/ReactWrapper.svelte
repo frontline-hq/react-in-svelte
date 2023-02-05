d82ff0833bd8306e9e10d4b582dea48d3e1f5dd2
Benjamin Preiss <benjaminpreiss@protonmail.com>
2023-02-05 18:48:44 +0100

    First version

---
+++ src/lib/ReactWrapper.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import {render as preactRender, hydrate as preactHydrate, type VNode} from 'preact';
+    import {render as preactPrerender} from 'preact-render-to-string'
+    export let vNode: VNode
+
+    const prerendered = preactPrerender(vNode as VNode<{}>)
+
+    function hydrate(node: HTMLElement, {vNode, prerendered}: {vNode: VNode, prerendered: string}) {
+        node.innerHTML = prerendered
+        preactHydrate(vNode, node)
+        return {
+            // Do I need to update here?
+			destroy() {
+				preactRender(null, node)
+			}
+		};
+    }
+</script>
+
+<div use:hydrate={{vNode, prerendered}}></div>
